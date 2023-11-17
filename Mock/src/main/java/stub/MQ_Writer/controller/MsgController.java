00fdb510e7537068cc519d64bb6305a0b4a092d8
DyadyushkaAu <z.kozhevnikoff102@yandex.ru>
2023-11-17 18:41:27 +0500

    Тестовое задание ВТБ

---
+++ Mock/src/main/java/stub/MQ_Writer/controller/MsgController.java
@@ -0,0 +1,25 @@
+package stub.MQ_Writer.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("msg")
+public class MsgController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @PostMapping
+    public void send(String msgId, String msg){
+        kafkaTemplate.send("msg", msgId, msg);
+    }
+}
+
---
+++ script.js
@@ -0,0 +1,49 @@
+import { check, group } from 'k6';
+import http from 'k6/http';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+
+
+export let options = {
+    scenarios: {
+        constant_request_rate: {
+          executor: 'constant-arrival-rate',
+          rate: 7,
+          timeUnit: '1s',
+          duration: '3s',
+          preAllocatedVUs: 5,
+          maxVUs: 10,
+        }
+      },
+      scenarios: {
+        constant_request_rate: {
+          executor: 'constant-arrival-rate',
+          rate: 15,
+          timeUnit: '1s',
+          duration: '3s',
+          preAllocatedVUs: 10,
+          maxVUs: 20,
+        }
+      },
+    thresholds: {
+        'http_req_duration': ['p(95)<1000']
+    },
+};
+
+export default function(){
+    let a = randomIntBetween(0, 100);
+    let b = randomIntBetween(0, 100);
+
+    group("write_to_kafka", function() {
+        group("KafkaWrite", function(){
+            let res = http.post('http://localhost:9000/msg',
+            {msgId: String(a), msg: String(b)});
+            console.log("msgId", a, "id", b);
+            check(res, {
+                "status code is 200": (res) => res.status == 200,
+            });
+            check(res, {
+                "status code is 404": (res) => res.status == 404,
+            });
+        });        
+    });
+};
